2bf23af7fafc7990da66b8c792258db529e6ffe1
Jakob Fix <jakob@jfix.com>
2017-12-03 22:34:04 +0100

    This is work in progress
    The idea is to refactor the calls for all projects and different
    URLs by using Promise.all().

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const async = require('async')
 const path = require('path')
-const dotenv = require('dotenv').config({path: path.resolve(__dirname, '_env')})
+require('dotenv').config({path: path.resolve(__dirname, '_env')})
+const async = require('async')
 const fs = require('fs')
 const rp = require('request-promise')
-const _ = require('lodash')
+const moment = require('moment')
 
 const projects = JSON.parse(fs.readFileSync(path.resolve(__dirname, process.env.CONFIG_FILE), 'utf8'))
 
 var resultObject = {}
-const dates = '%3E%3C2017-11-12|2017-11-19'
+
+const fromDate = moment().day(-7).format('YYYY-MM-DD')
+const toDate = moment().day(0).format('YYYY-MM-DD')
+const dates = `%3E%3C${fromDate}|${toDate}`
 const requestOptions = {
   'headers': {
     'X-Redmine-API-Key': process.env.REDMINE_API_KEY
@@ -17,100 +20,61 @@ const requestOptions = {
   'url': ''
 }
 
-async.each(
-  // collection to iterate over
-  projects,
-
-  // function to execute for each item
-  (project, cb) => {
-    // console.log(`now processing ${project.name} ...`)
-    const name = project.name
-    const trackerIds = project.stock_ticket_types.join('|')
-    const url = `${process.env.REDMINE_URL}?project_id=${project.id}&status_id=open&tracker_id=${trackerIds}`
-    requestOptions.url = url
-    rp(requestOptions)
-      .then((res) => {
-        if (resultObject[name]) {
-          resultObject[name].open_issues = res.total_count
-        } else {
-          resultObject[name] = {'open_issues': res.total_count}
-        }
-        console.log(`Counting ${res.total_count} for ${name}.`)
-        cb()
-      })
-      .catch((err) => {
-        console.log(err.message)
-        cb()
-      })
-  },
-
-  // once all items finished
-  (err) => {
-    if (err) {
-      console.log(`ERR: ${err}`)
-    } else {
-      // add stock
-      async.reduce(resultObject, 0, (memo, i, cb) => {
-        process.nextTick(function () {
-          cb(null, memo + i.open_issues)
+const getStockTickets = function getTickets (project) {
+  return new Promise(
+    (resolve, reject) => {
+      const name = project.name
+      const trackerIds = project.stock_ticket_types.join('|')
+      const url = `${process.env.REDMINE_URL}?project_id=${project.id}&status_id=open&tracker_id=${trackerIds}`
+      requestOptions.url = url
+      rp(requestOptions)
+        .then((res) => {
+          if (resultObject[name]) {
+            resolve(resultObject[name].open_issues = res.total_count)
+          } else {
+            resolve(resultObject[name] = {'open_issues': res.total_count})
+          }
         })
-      },
-      (err, res) => {
-        if (err) {
-          console.log(`ERR: ${err}`)
-        } else {
-          console.log(`Open issues SUM: ${res}`)
-        }
-      })
+        .catch((err) => reject(
+          Error(err.message)
+        )
+      )
+      // reject(Error('error'))
     }
-  }
-)
+  )
+}
 
-async.each(
-  // collection to iterate over
-  projects,
-
-  // function to execute for each item
-  (project, cb) => {
-    // console.log(`now processing ${project.name} ...`)
-    const name = project.name
-    const trackerIds = project.stock_ticket_types.join('|')
-    const url = `${process.env.REDMINE_URL}?project_id=${project.id}&created_on=${dates}&tracker_id=${trackerIds}`
-    requestOptions.url = url
-    rp(requestOptions)
-      .then((res) => {
-        if (resultObject[name]) {
-          resultObject[name].stock = res.total_count
-        } else {
-          resultObject[name] = {'stock': res.total_count}
-        }
-        console.log(`Counting ${res.total_count} for ${name}.`)
-        cb()
-      })
-      .catch((err) => {
-        console.log(err.message)
-        cb()
-      })
-  },
-
-  // once all items finished
-  (err) => {
-    if (err) {
-      console.log(`ERR: ${err}`)
-    } else {
-      // add stock
-      async.reduce(resultObject, 0, (memo, i, cb) => {
-        process.nextTick(function () {
-          cb(null, memo + i.stock)
+const getNewTickets = function getNewTickets (project) {
+  return new Promise(
+    (resolve, reject) => {
+      const name = project.name
+      const trackerIds = project.stock_ticket_types.join('|')
+      const url = `${process.env.REDMINE_URL}?project_id=${project.id}&status_id=open&tracker_id=${trackerIds}`
+      requestOptions.url = url
+      resolve(
+        rp(requestOptions)
+        .then((res) => {
+          if (resultObject[name]) {
+            resultObject[name].open_issues = res.total_count
+          } else {
+            resultObject[name] = {'open_issues': res.total_count}
+          }
+          console.log(`Counting ${res.total_count} for ${name}.`)
         })
-      },
-      (err, res) => {
-        if (err) {
-          console.log(`ERR: ${err}`)
-        } else {
-          console.log(`Stock SUM: ${res}`)
-        }
-      })
+        .catch((err) => {
+          console.log(err.message)
+        })
+      )
+      reject(Error('error'))
     }
-  }
-)
+  )
+}
+
+async function getAll () {
+  const stockItems = await Promise.all(
+    getStockTickets
+  )
+  console.log(stockItems)
+}
+
+getAll()
